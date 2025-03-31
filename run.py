646f5b05337baae9edd00d25f761629896316e61
Nearez <97766579+Nearezz@users.noreply.github.com>
2025-03-31 13:56:05 -0400

    Fixed a bug

--- run.py
+++ run.py
@@ -13,7 +13,7 @@ async def close_positions(trader,ticker):
     item = trader.get_portfolio_item(ticker)
     await close_all_positions(trader, ticker, item)
 
-async def process_ticker(trader:shift.Trader, ticker:str, data:dict, h:float,order_table:list):
+async def process_ticker(trader:shift.Trader, ticker:str, data:dict, upper_bound:float,lower_bound:float,order_table:list):
     try:
         print(f"Processing ticker: {ticker}")
         item = trader.get_portfolio_item(ticker)
@@ -29,7 +29,7 @@ async def process_ticker(trader:shift.Trader, ticker:str, data:dict, h:float,ord
         first = data["first_price"]
 
         
-        if not (5 <= p_fluc <= h):
+        if not (lower_bound <= p_fluc <= upper_bound):
             print(f"{ticker} skipped: fluctuation {p_fluc:.2f}")
             return
         print(f"{ticker} fluctuation: {p_fluc:.2f}, placing limit orders")
@@ -72,7 +72,8 @@ async def main():
     tickers_list = trader.get_stock_list()
     start_time = datetime.now()
     end_time = start_time + timedelta(minutes=30)
-    h = 7
+    upper_bound = 7
+    lower_bound = 5
     loop_count = 0
     order_table = []
     while datetime.now() < end_time:
@@ -82,7 +83,7 @@ async def main():
         print(f"Fetched Pickens data at {datetime.now().strftime('%H:%M:%S')}")
 
         tasks = [
-            process_ticker(trader, ticker, pickens_dict[ticker], h,order_table)
+            process_ticker(trader, ticker, pickens_dict[ticker],upper_bound,lower_bound,order_table)
             for ticker in pickens_dict
         ]
         await asyncio.gather(*tasks)
@@ -94,8 +95,13 @@ async def main():
         order_table.clear()
         print("All pending orders cancelled.\n")
 
-    close_out_tasks = [close_positions(trader, ticker) async for ticker in tickers_list]
+   
+
+
+    
+    close_out_tasks = [close_positions(trader, ticker) for ticker in tickers_list]
     await asyncio.gather(*close_out_tasks)
+
 
     trader.disconnect()
     print("Session complete. Disconnected.")
